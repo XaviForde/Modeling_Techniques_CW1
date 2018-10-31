0bf61fb4977c6ff8b20d34d1ce15e4dca4cc138d
XaviForde <xav_mister_General@hotmail.co.uk>
2018-10-31 17:08:04 +0000

    Part a of courswork completed using LaplaceElemMatrix.m which gets the local element matrix for a given element.

---
+++ LaplaceElemMatrix.m
@@ -0,0 +1,24 @@
+function [SqMatrix] = LaplaceElemMatrix(D, eID, msh)
+
+%Returns a local 2x2 element matrix for a given element
+% Inputs: 
+% D - Coefficient of Diffusion
+% eID - Index of element within mesh structure
+% msh - Mesh which contains local elements within it's structure
+
+%% Local element matrix for J = 1
+SqMatrix = zeros(2,2);
+SqMatrix(1,1) = 0.5*D;
+SqMatrix(1,2) = -0.5*D;
+SqMatrix(2,1) = -0.5*D;
+SqMatrix(2,2) = 0.5*D;
+
+%% To get local element matrix for this element multiply by dZeta/dx which
+%is equivalent to 1/J 
+
+J = msh.elem(eID).J;  %Get Jacobian for the element
+SqMatrix = (1/J) .* SqMatrix;   %Local element matrix for element eID
+
+end
+
+
